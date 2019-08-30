66f793a413b3e2098231e58a2a8e26987c4e4523
Garrett Thompson <gtcruz.gt@gmail.com>
2019-08-30 02:54:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
-
-html = open("https://flatironschool.com/")
-
-#doc = Nokogiri::HTML(html)
-
-#puts doc
-
-
-doc = Nokogiri::HTML(open("http://flatironschool.com/"))
-puts doc.css(".site-header__hero__headline").text
+ 
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+ 
+doc.css(".tout__label.heading.heading--level-4")
